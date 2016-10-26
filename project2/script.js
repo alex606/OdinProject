2e2d06cc315bdea7b590d92f8162d783050aa473
Alex Wu <alex606@att.net>
2016-10-26 00:00:03 -0500

    Generated tags with jQuery

--- project2/script.js
+++ project2/script.js
@@ -1,5 +1,12 @@
 $(document).ready( function(){
-	$('.cell').hover( function(){
-		$(this).hide();
+
+	var $row = "<div class='rowElement'></div>";
+	var $col = "<div class='cellElement'></div>";
+
+	$('.newBoard').append($row).append($row).append($row).append($row);
+	$('.rowElement').append($col).append($col).append($col).append($col);
+
+	$('.cellElement').hover( function(){
+		$(this).addClass("hovered")
 	});
 });
