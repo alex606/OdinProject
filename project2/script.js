1a41f97182e23f919fa93f7c07d0dcf7eb502cc2
Alex Wu <alex606@att.net>
2016-10-26 00:13:06 -0500

    Added button control logic for making new board

--- project2/script.js
+++ project2/script.js
@@ -3,10 +3,26 @@ $(document).ready( function(){
 	var $row = "<div class='rowElement'></div>";
 	var $col = "<div class='cellElement'></div>";
 
+	// Creates 4 x 4 board on start
 	$('.newBoard').append($row).append($row).append($row).append($row);
 	$('.rowElement').append($col).append($col).append($col).append($col);
 
 	$('.cellElement').hover( function(){
 		$(this).addClass("hovered")
+	});
+
+	$('button').click( function(){
+		var newRows = prompt("Enter number of rows");
+		var newCols = prompt("Enter number of columns");
+		$( ".rowElement" ).remove();
+
+		for (i = 0; i < newRows; i++)
+		{
+			$('.newBoard').append($row);
+		}
+	    for(j = 0; j < newCols; j++)
+		{
+			$('.rowElement').append($col);
+		}
 	});
 });
