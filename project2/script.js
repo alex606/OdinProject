b2091e5a57bfbf9cb7e4feba709394a6f8a6bc3a
Alex Wu <alex606@att.net>
2016-10-28 22:30:07 -0500

    Added trail and darken effects

--- project2/script.js
+++ project2/script.js
@@ -4,16 +4,22 @@ $(document).ready( function(){
 	var $col = "<div class='cellElement'></div>";
 
 	$('button').click( function(){
-		testFunction()
+		BuildGrid()
 	});
 
-	var testFunction = function()
+	var BuildGrid = function()
 	{
+		// For use with Trail option - Black background for opacity
+		$('.newBoard').css('background-color', '#000');
+
+		// Grabs values for new rows and columns
 		var newRows = $('input[name="rowNumber"]').val();
 		var newCols = $('input[name="colNumber"]').val();
 
+		// Clears current board
 		$( ".rowElement" ).remove();
 
+		// For loops provide construction of new grid
 		for (i = 0; i < newRows; i++)
 		{
 			$('.newBoard').append($row);
@@ -23,19 +29,29 @@ $(document).ready( function(){
 			$('.rowElement').append($col);
 		}
 
+		// Resize cells elements as necessary and accounts for cell borders
 		var newHeight = ($('.newBoard').height() / newRows - 2)
 		$('.rowElement').css('height', newHeight);
 		$('.cellElement').css('height', newHeight);
-
 		var newWidth = (($('.newBoard').width() / newCols - 2))
 		$('.cellElement').css('width', newWidth);
-		
-		$('.cellElement').hover( function(){
-			//$(this).addClass("hovered")
+
+		// Where dropping the effects occur		
+		$('.cellElement').mouseenter( function(){
 			var color = $('#color').val();
-			if(color == 'Random')
+			if(color === 'Random')
 			{
 				color = GetRandomColor();
+			}
+			else if(color === 'trail')
+			{
+				$(this).css('opacity',0);
+				$(this).fadeTo('slow', 1);
+			}
+			else if(color === 'darken')
+			{
+				var newOpacity = $(this).css("opacity") * 0.75;
+				$(this).css("opacity", newOpacity);
 			}
 			$(this).css('background', color);
 		});
